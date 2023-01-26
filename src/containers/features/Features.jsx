1c0807bb48fd01f61a7f705c9050e029ca095141
Alex <multiwebagencia@gmail.com>
2023-01-25 21:30:48 -0300

    s

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -25,8 +25,8 @@ const Features = () => {
   return (
     <div className="gpt3__features section__padding" id="features">
       <div className="gpt3__features-heading">
-        <h1 className="gradient__text">The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h1>
-        <p>Request Early Access to Get Started</p>
+        <h1 className="gradient__text">O Futuro é Agora e Você Só Precisa Concretizá-lo. Entre no futuro hoje e faça acontecer.</h1>
+        <p>Solicite acesso antecipado para começar</p>
       </div>
       <div className="gpt3__features-container">
         <div>
